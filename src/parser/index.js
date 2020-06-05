95a25791e54a6b24202093f0868ba1a3ed0b5228
adlermo <adlerorteney@gmail.com>
2020-06-05 13:28:33 -0300

    removed: console logs

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,15 +1,19 @@
-const fs = require('fs');
+module.exports = retornaObjetoKills = (() => {
+    const fs = require('fs');
 
-const retornaObjetoKills = () => {
+    // Reads the gamelog file
     let logFile = fs.readFileSync('./log/games.log').toString();
     logFile = logFile.split('\n');
 
+    // Global vars to be used during all object array creation
     let gameData = [];
-    let gameId = 0;
+    let gameId = -1;
     let deaths = 0;
     let kills = 0;
 
+    // Iterates over all file lines to retrieve data about kills
     for (var line of logFile) {
+        // Creates new object when detects a new game is started
         if (line.includes("InitGame:")) {
             gameId++;
             let parser = `{
@@ -22,7 +26,7 @@ const retornaObjetoKills = () => {
             gameData.push(JSON.parse(parser));
         };
 
-        // Adds data from the log file to the array 
+        // Adds kills data from the line file to the object created previously
         if (line.includes("Kill:")) {
             let regExp = /:\s(\D.*)/gm;
             let killDescription = regExp.exec(line)[1];
@@ -34,32 +38,27 @@ const retornaObjetoKills = () => {
                 deaths = gameData[gameId].game_data.kills[`${killed}`] ? gameData[gameId].game_data.kills[`${killed}`] - 1 : -1;
 
                 gameData[gameId].game_data.kills[`${killed}`] = deaths;
-
-                console.log(gameData[gameId].game_data.kills[`${killed}`])
             } else {
                 kills = gameData[gameId].game_data.kills[`${killer}`] ? gameData[gameId].game_data.kills[`${killer}`] + 1 : 1;
 
 
                 gameData[gameId].game_data.kills[`${killer}`] = kills;
-
-                console.log(gameData[gameId].game_data.kills[`${killer}`])
             };
 
-            gameData[gameId].game_data.total_kills += 1;
+            gameData[gameId].game_data.total_kills++;
         };
     };
 
+    // Pushes all players retrieved in dictionary to the list PLAYERS
     for (let game of gameData) {
         for (let player in game.game_data.kills) {
             game.game_data.players.push(player);
         };
-    }
+    };
 
-
+    // Writes the file
     fs.writeFileSync("./src/game_data.json", JSON.stringify(gameData));
 
+    // Returns total of games created
     return "There was " + gameData.length + " games";
-};
-
-
-console.log(retornaObjetoKills());
+})();
